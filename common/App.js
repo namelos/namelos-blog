7e7d8b8bb134472b38751e6f00692af9e372d08c
Fred Wang <namelos@outlook.com>
2017-03-26 23:16:16 +0800

    server render react component

---
+++ common/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <h2>Hi.</h2>
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import App from '../common/App'
 
 const app = express()
 
-app.get('/', (req, res) =>
+app.get('/api', (req, res) =>
   res.send('hi.'))
+
+app.get('*', (req, res) => {
+  let application = renderToString(<App />)
+
+  let html = `
+  <div id="root">${application}</div>
+  `
+
+  res.send(html)
+})
 
 export default app

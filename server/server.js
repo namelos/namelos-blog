ae85dcf18131fe7b788e7493128611f9d0dc42bf
Fred Wang <namelos@outlook.com>
2017-03-29 22:35:38 +0800

    remove redundant server side codes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,11 @@
 import express from 'express'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import App from '../common/App'
 
 const app = express()
 
-app.get('/api', (req, res) =>
-  res.send('hi.'))
-
 app.get('*', (req, res) => {
-  let application = renderToString(<App />)
-
   let html = `
-  <div id="root">${application}</div>
-  <script src="http://localhost:3001/client.js"></script>
+    <div id="root"></div>
+    <script src="http://localhost:3001/client.js"></script>
   `
 
   res.send(html)
